965e831c2855245dee8b0a5ef39ed955fef37a38
Tagliapietra96 <tagliatek96@gmail.com>
2022-11-23 14:10:17 +0100

    created basic files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const { createApp } = Vue;
+createApp({
+    // ************ VARIABILI ************
+    data() {
+        return {
+            //STRINGHE
+
+            //NUMERI
+
+            //BOOLEANI
+
+            //ARRAY
+
+            //OGGETTI
+
+        }
+    },
+    // ************ FUNZIONI ************
+    methods: {
+
+    },
+	mounted(){
+	
+	}
+}).mount('#my-app');
